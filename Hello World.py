315e6bba5b5371a01620748fec2e67f449b36699
embedstar <160602901+embedstar@users.noreply.github.com>
2025-12-10 10:57:17 +0400

    Hello World.py
    Start with Python....

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+# This is a comment. It will not be executed.
+print("Hello World!")
+
+# print() is a built-in Python function that instructs the computer to display text on the screen.
+# is used to indicate a comment
+# "Hello World!" is a sequence of text called a string. In Python, strings are enclosed in quotes (either single ' or double ").
+
+"""This is a multiline comment
+Used for more than one line comments 
+In larger codes
+All codes entered within # and """ will not be printed on screen.
+"""
